ac65550d07c14072b6d9de22c3914dbf49371d0e
CarHerrera <40675279+CarHerrera@users.noreply.github.com>
2024-12-05 16:56:23 +0000

    Got Base Express Working

---
+++ index.js
@@ -0,0 +1,18 @@
+const {MongoClient, ServerApiVersion} = require('mongodb');
+const express = require('express');
+const path = require("path");
+const bodyParser = require('body-parser');
+// require("dotenv").config({path: path.resolve(__dirname, '.env')});
+const app = express();
+const fs = require('fs');
+let port = 8000;
+
+app.set("views", path.resolve(__dirname, "templates")); 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:false})); 
+app.get('/', (req,res) =>{
+    res.render('home');
+})
+
+app.listen(port);
+console.log(`Listening on Port ${port}`);
